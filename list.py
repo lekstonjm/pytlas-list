8f754f47020db2860b17ce06f1cefaa5b6151edd
jmlekston <conso.lekston@gmail.com>
2018-12-17 14:16:35 +0100

    add list, display list, delete list

--- list.py
+++ list.py
@@ -6,21 +6,13 @@ import re
 # This entity will be shared among training data since it's not language specific
 
 """
-%[add_list]
-  add @[item_name] in list @[list_name]
-  add in @[list_name]
-
 %[remove_list]
   remove @[item_name] from @[list_name]
 
-%[delete_list]
-  delete list @[list_name]
-
-
-%[list_content]
-  list content of @[list_name]
-  enumerate @[list_name]
-  what have I in @[list_name]
+%[send_list]
+  send me the list @[list_name]
+  send the list @[list_name] to @[email] 
+  send @[list_name] to @[email]
 """
 
 
@@ -28,15 +20,32 @@ import re
 def en_data(): return """
 %[create_list]
   create a new list named @[list_name]
-  create a list
+  create a new list @[list_name]
+  create a list @[list_name]
   add a list
   new list with name @[list_name]
+
+%[delete_list]
+  delete list @[list_name]
+  delete my list @[list_name]
+  erase @[list_name]
+  destroy @[list_name]
+  put @[list_name] in the trash
+  put @[list_name] in the can
+
 
 %[add_list]
   add @[item_name] in list @[list_name]
   add @[item_name] in my @[list_name]
-  add @[item_name] in @[list_name]
-  I want add something in may @[list_name]
+  add @[item_name] in the @[list_name]
+  I want add something in my @[list_name]
+  could you  add something in the @[list_name]
+
+%[display_list]
+show me the @[list_name] content
+show me my @[list_name]
+enumerate my @[list_name]
+what I have in my @[list_name]
 
 @[list_name]
   shoping list
@@ -76,36 +85,79 @@ def slugify(value):
 def default_pytlas_list_path():
   return os.path.join(os.path.expanduser('~'),'.pytlas-list')
 
+def build_list_file_path(list_name, list_dir_path):
+  list_filename = slugify(list_name)+'.json'  
+  list_path = os.path.join(list_dir_path,list_filename)
+  return list_path
+
+def create_blank_file(list_name, list_path):
+  list_content = { 'name' : list_name, 'items' : [] }
+  with open(list_path,'w') as json_file:
+    json.dump(list_content, json_file)
+
+def append_file(item_name, list_path):
+  list_content = {}
+  with open(list_path,'r') as json_file:
+    list_content = json.load(json_file)
+    list_content['items'].append(item_name)
+  with open(list_path,'w') as json_file:
+    json.dump(list_content, json_file)
 
 @intent('create_list')
 def on_create_list(req):
   list_name = req.intent.slot('list_name').first().value
   if not list_name:
     return req.agent.ask('list_name',req._('Please choose a name for your list.'))
-  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')
 
+  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')  
   if not list_dir_path:
     list_dir_path = default_pytlas_list_path()
   
   if not os.path.exists(list_dir_path):
     os.makedirs(list_dir_path)
-  
-  list_filename = slugify(list_name)+'.json'  
-  list_path = os.path.join(list_dir_path,list_filename)
+
+  list_path = build_list_file_path(list_name, list_dir_path)
   
   if os.path.exists(list_path):
     req.agent.answer(req._('Oops! This list already exists.'))
     return req.agent.done()
 
-  with open(list_path,'w') as json_file:
-    list_content = { 'name' : list_name, 'items' : [] }
-    json.dump(list_content, json_file)
+  try:  
+    create_blank_file(list_name, list_path)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"'.format(list_path)))
+    return req.agent.done()    
 
-  req.agent.answer(req._('Ok your list has been created.'))
+  req.agent.answer(req._('Ok! your list "{0}" has been created.'.format(list_name)))
   return req.agent.done()
 
+@intent('delete_list')
+def on_delete_list(req):
+  list_name = req.intent.slot('list_name').first().value
+  if not list_name:
+    return req.agent.ask('list_name',req._('In which list?'))
+  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')  
+  if not list_dir_path:
+    list_dir_path = default_pytlas_list_path()
+  
+  list_path = build_list_file_path(list_name, list_dir_path)
+  if os.path.exists(list_path):
+    delete_confirmed = req.intent.slot('delete_confirmed').first().value
+    yes = req._('Yes')
+    no = req._('No')
+    if not delete_confirmed:
+      return req.agent.ask('delete_confirmed', req._('Would you like delete "{0}"?').format(list_name), [yes, no])
+    if delete_confirmed == yes:
+      try:
+        os.remove(list_path)
+      except:
+        req.agent.answer(req._('Oops! Something bad append. I can\'t delete file "{0}"'.format(list_path)))
+        return req.agent.done()    
+  return req.agent.done()    
+
+
 @intent('add_list')
-def on_add_item(req):
+def on_add_list(req):
   list_name = req.intent.slot('list_name').first().value
   if not list_name:
     return req.agent.ask('list_name',req._('In which list?'))
@@ -114,25 +166,59 @@ def on_add_item(req):
   if not list_dir_path:
     list_dir_path = default_pytlas_list_path()
   
-  list_filename = slugify(list_name)+'.json'  
-  list_path = os.path.join(list_dir_path,list_filename)
+  list_path = build_list_file_path(list_name, list_dir_path)
+  
   if not os.path.exists(list_path):
-    req.agent.answer(req._('Hummm! This list does\'t exists.'))
-    return req.agent.done()
+    create_confirmed = req.intent.slot('create_confirmed').first().value
+    yes = req._('Yes')
+    no = req._('No')
+    if not create_confirmed:
+      return req.agent.ask('create_confirmed', req._('Hummm! The list "{0}" seems not exists. Would you like create it?').format(list_name), [yes, no])
+    if create_confirmed == yes:
+      try:
+        create_blank_file(list_name, list_path)
+      except:
+        req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"'.format(list_path)))
+        return req.agent.done()    
+    else:
+      return req.agent.done()    
 
   item_name = req.intent.slot('item_name').first().value
   if not item_name:
     return req.agent.ask('item_name', req._('What do you want to add in your list?'))
 
-  list_content = { 'name' : list_name, 'items' : [] }
-  with open(list_path,'r') as json_file:
-    list_content = json.load(json_file)
-    list_content['items'].append(item_name)
+  try:
+    append_file(item_name, list_path)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t add "{0}" in file "{1}"'.format(item_name, list_path)))
+    return req.agent.done()    
 
-  with open(list_path,'w') as json_file:
-    json.dump(list_content, json_file)
-
-  req.agent.answer(req._('Ok, item has been added in your list'))
+  req.agent.answer(req._('Ok, "{0}" has been added in your list "{1}"').format(item_name, list_name))
   return req.agent.done()
+
+@intent('display_list')
+def on_display_list(req):
+  list_name = req.intent.slot('list_name').first().value
+  if not list_name:
+    return req.agent.ask('list_name',req._('Which list?'))
   
+  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')  
+  if not list_dir_path:
+    list_dir_path = default_pytlas_list_path()
+  
+  list_path = build_list_file_path(list_name, list_dir_path)
+  
+  if not os.path.exists(list_path):
+    req.agent.answer(req._('Hummm! The list "{0}" seems not exists.').format(list_name))
+    return req.agent.done()
+  
+  list_content = {}
+  try:
+    with open(list_path,'r') as json_file:
+      list_content = json.load(json_file)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t open the file "{0}"').format(list_path))
+    return req.agent.done()    
+  req.agent.answer(req._('Your list "{0}" contains : {1}').format(list_content['name'], ','.join(list_content['items'])))
+  return req.agent.done()    
 
