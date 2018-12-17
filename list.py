8eaa830217e12244a05ba0745367390deda499e2
lekstonjm <lekston.jm@gmail.com>
2018-12-17 21:22:52 +0100

    add remove item, send (work in progress)

--- list.py
+++ list.py
@@ -3,16 +3,11 @@ import json
 import os
 import unicodedata
 import re
+import smtplib
 # This entity will be shared among training data since it's not language specific
 
 """
-%[remove_list]
-  remove @[item_name] from @[list_name]
 
-%[send_list]
-  send me the list @[list_name]
-  send the list @[list_name] to @[email] 
-  send @[list_name] to @[email]
 """
 
 
@@ -30,22 +25,29 @@ def en_data(): return """
   delete my list @[list_name]
   erase @[list_name]
   destroy @[list_name]
-  put @[list_name] in the trash
   put @[list_name] in the can
+  trash list @[list_name]
 
-
-%[add_list]
+%[add_item]
   add @[item_name] in list @[list_name]
   add @[item_name] in my @[list_name]
   add @[item_name] in the @[list_name]
   I want add something in my @[list_name]
   could you  add something in the @[list_name]
 
+%[remove_item]
+  remove @[item_name] from @[list_name]
+
 %[display_list]
 show me the @[list_name] content
 show me my @[list_name]
 enumerate my @[list_name]
 what I have in my @[list_name]
+
+%[send_list]
+  send me the list @[list_name]
+  send the list @[list_name] to @[email] 
+  send @[list_name] to @[email]
 
 @[list_name]
   shoping list
@@ -61,7 +63,11 @@ what I have in my @[list_name]
   call mother
   bread
   eggs
-  trip  
+  trip
+
+@[email]
+  first.name@domain.com
+
 """
 
 #@training('fr')
@@ -95,11 +101,19 @@ def create_blank_file(list_name, list_path):
   with open(list_path,'w') as json_file:
     json.dump(list_content, json_file)
 
-def append_file(item_name, list_path):
+def append_in_file(item_name, list_path):
   list_content = {}
   with open(list_path,'r') as json_file:
     list_content = json.load(json_file)
     list_content['items'].append(item_name)
+  with open(list_path,'w') as json_file:
+    json.dump(list_content, json_file)
+
+def remove_from_file(item_name, list_path):
+  list_content = {}
+  with open(list_path,'r') as json_file:
+    list_content = json.load(json_file)
+    list_content['items'] = [x for x in list_content['items'] if x != item_name]
   with open(list_path,'w') as json_file:
     json.dump(list_content, json_file)
 
@@ -125,10 +139,10 @@ def on_create_list(req):
   try:  
     create_blank_file(list_name, list_path)
   except:
-    req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"'.format(list_path)))
+    req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"').format(list_path))
     return req.agent.done()    
 
-  req.agent.answer(req._('Ok! your list "{0}" has been created.'.format(list_name)))
+  req.agent.answer(req._('Ok! your list "{0}" has been created.').format(list_name))
   return req.agent.done()
 
 @intent('delete_list')
@@ -141,23 +155,27 @@ def on_delete_list(req):
     list_dir_path = default_pytlas_list_path()
   
   list_path = build_list_file_path(list_name, list_dir_path)
-  if os.path.exists(list_path):
-    delete_confirmed = req.intent.slot('delete_confirmed').first().value
-    yes = req._('Yes')
-    no = req._('No')
-    if not delete_confirmed:
-      return req.agent.ask('delete_confirmed', req._('Would you like delete "{0}"?').format(list_name), [yes, no])
-    if delete_confirmed == yes:
-      try:
-        os.remove(list_path)
-      except:
-        req.agent.answer(req._('Oops! Something bad append. I can\'t delete file "{0}"'.format(list_path)))
-        return req.agent.done()    
+  if not os.path.exists(list_path):
+    req.agent.answer(req._('Hummm! The list "{0}" seems not exists').format(list_name))
+    return req.agent.done()    
+
+  delete_confirmed = req.intent.slot('delete_confirmed').first().value
+  yes = req._('Yes')
+  no = req._('No')
+  if not delete_confirmed:
+    return req.agent.ask('delete_confirmed', req._('Would you like delete "{0}"?').format(list_name), [yes, no])
+  
+  if delete_confirmed == no:
+    return req.agent.done()    
+  
+  try:
+    os.remove(list_path)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t delete file "{0}"').format(list_path))
   return req.agent.done()    
 
-
-@intent('add_list')
-def on_add_list(req):
+@intent('add_item')
+def on_add_item(req):
   list_name = req.intent.slot('list_name').first().value
   if not list_name:
     return req.agent.ask('list_name',req._('In which list?'))
@@ -174,13 +192,14 @@ def on_add_list(req):
     no = req._('No')
     if not create_confirmed:
       return req.agent.ask('create_confirmed', req._('Hummm! The list "{0}" seems not exists. Would you like create it?').format(list_name), [yes, no])
-    if create_confirmed == yes:
-      try:
-        create_blank_file(list_name, list_path)
-      except:
-        req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"'.format(list_path)))
-        return req.agent.done()    
-    else:
+    
+    if create_confirmed == no:
+      return req.agent.done()    
+
+    try:
+      create_blank_file(list_name, list_path)
+    except:
+      req.agent.answer(req._('Oops! Something bad append. I can\'t create file "{0}"').format(list_path))
       return req.agent.done()    
 
   item_name = req.intent.slot('item_name').first().value
@@ -188,13 +207,43 @@ def on_add_list(req):
     return req.agent.ask('item_name', req._('What do you want to add in your list?'))
 
   try:
-    append_file(item_name, list_path)
+    append_in_file(item_name, list_path)
   except:
-    req.agent.answer(req._('Oops! Something bad append. I can\'t add "{0}" in file "{1}"'.format(item_name, list_path)))
+    req.agent.answer(req._('Oops! Something bad append. I can\'t add "{0}" in file "{1}"').format(item_name, list_path))
     return req.agent.done()    
 
   req.agent.answer(req._('Ok, "{0}" has been added in your list "{1}"').format(item_name, list_name))
   return req.agent.done()
+
+@intent('remove_item')
+def on_remove_item(req):
+  list_name = req.intent.slot('list_name').first().value
+  if not list_name:
+    return req.agent.ask('list_name',req._('From which list?'))
+
+  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')  
+  if not list_dir_path:
+    list_dir_path = default_pytlas_list_path()
+  
+  list_path = build_list_file_path(list_name, list_dir_path)
+  if not os.path.exists(list_path):
+    req.agent.answer(req._('Hummm! The list "{0}" seems not exists').format(list_name))
+    return req.agent.done()    
+
+  item_name = req.intent.slot('item_name').first().value
+  if not item_name:
+    return req.agent.ask('item_name', req._('What do you want remove from your list?'))
+
+  try:
+    remove_from_file(item_name, list_path)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t remove "{0}" from file "{1}"').format(item_name, list_path))
+    return req.agent.done()    
+
+  req.agent.answer(req._('Ok, "{0}" has been removed from your list "{1}"').format(item_name, list_name))
+  return req.agent.done()
+
+
 
 @intent('display_list')
 def on_display_list(req):
@@ -219,6 +268,49 @@ def on_display_list(req):
   except:
     req.agent.answer(req._('Oops! Something bad append. I can\'t open the file "{0}"').format(list_path))
     return req.agent.done()    
+
   req.agent.answer(req._('Your list "{0}" contains : {1}').format(list_content['name'], ','.join(list_content['items'])))
   return req.agent.done()    
+
+@intent('send_list')
+def on_send_list(req):
+  list_name = req.intent.slot('list_name').first().value
+  if not list_name:
+    return req.agent.ask('list_name',req._('Which list?'))
+  
+  list_dir_path = req.agent.meta.get('PYTLAS-LIST-PATH')  
+  if not list_dir_path:
+    list_dir_path = default_pytlas_list_path()
+  
+  list_path = build_list_file_path(list_name, list_dir_path)
+  
+  if not os.path.exists(list_path):
+    req.agent.answer(req._('Hummm! The list "{0}" seems not exists.').format(list_name))
+    return req.agent.done()
+  
+  if not req.agent.meta.get('PYTLAS-LIST-SMTP') or \
+  not req.agent.meta.get('PYTLAS-LIST-FROM') or \
+  not req.agent.meta.get('PYTLAS-LIST-LOGIN') or \
+  not req.agent.meta.get('PYTLAS-LIST-PWD'):
+    req.agent.answer(req._('Hummm! Credentials are requiered to send email.'))
+    return req.agent.done()
+
+  list_content = {}
+  try:
+    with open(list_path,'r') as json_file:
+      list_content = json.load(json_file)
+  except:
+    req.agent.answer(req._('Oops! Something bad append. I can\'t open the file "{0}"').format(list_path))
+    return req.agent.done()    
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+
+    #Next, log in to the server
+    server.login("youremailusername", "password")
+
+    #Send the mail
+    msg = """
+    
+    Hello!""" # The /n separates the message from the headers
+    server.sendmail("you@gmail.com", "target@example.com", msg)
 
